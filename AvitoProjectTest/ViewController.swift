ede5755730b45f0a352fb8da6cebd8fd43d87b0d
Zakaria <zakariaelmouatassim@MacBook-Pro-de-t.local>
2018-03-05 20:53:03 +0000

    Initial Commit

---
+++ AvitoProjectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvitoProjectTest
+//
+//  Created by Zakaria elmouatassim on 05/03/2018.
+//  Copyright © 2018 Zakaria elmouatassim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
